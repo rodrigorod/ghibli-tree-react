9b4f5430c1b42986fbc4d13db26a738104fd09d4
Rodrigo Carraco Rodrigues <rcarracorodrigues@gmail.com>
2023-03-25 13:00:08 +0000

    Initial commit

---
+++ src/components/Trees.jsx
@@ -0,0 +1,37 @@
+import React, { forwardRef, useMemo } from "react";
+import { useGLTF } from  "@react-three/drei";
+import { Vector3 } from "three";
+import { GhibliShader } from "../GhibliShader";
+
+export const Trees = forwardRef((props, ref) => {
+  const { nodes } = useGLTF("/trees.glb");
+
+  const uniforms = useMemo(() => {
+    return {
+      colorMap: {
+        value: props.colors,
+      },
+      brightnessThresholds: {
+        value: [0.6, 0.35, 0.001]
+      },
+      lightPosition: {
+        value: new Vector3(15, 15, 15),
+      }
+    }
+  }, [props.colors])
+
+  return (
+    <group ref={ref} {...props} dispose={null}>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Foliage.geometry}
+        position={[0.33, -0.05, -0.68]}
+      >
+        <shaderMaterial attach="material" {...GhibliShader} uniforms={uniforms} />
+      </mesh>
+    </group>
+  )
+});
+
+useGLTF.preload("/trees.glb");
